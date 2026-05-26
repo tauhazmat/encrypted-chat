fc444af2a0443ef80af765535efa33a7c8946677
Tauha Azmat <160022078+tauhazmat@users.noreply.github.com>
2026-05-26 22:02:41 +0500

    chat connecting 2 users done (encryption left)
    This encrypted chat connects 2 users and lets them chat but asymmetric encryption needs to be added

---
+++ main.py
@@ -0,0 +1,40 @@
+import socket
+import threading
+
+import rsa
+
+
+choice = input("Do you want to host (1) or to connect (2): ")
+if choice == "1":
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(("10.230.218.121", 9999))
+    server.listen()
+
+
+    client, _ = server.accept()
+
+elif choice == "2":
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(("10.230.218.121", 9999))
+else:
+    exit()
+
+
+
+
+def sending_messages(c):
+    while True:
+        message = input("")
+        c.send(message.encode())
+        print("You: " + message)
+
+
+
+def receiving_messages(c):
+    while True:
+        message = input("")
+        print("Partener: " + c.recv(1024).decode())
+
+
+threading.Thread(target=sending_messages, args=(client,)).start()
+threading.Thread(target=receiving_messages, args=(client,)).start()
