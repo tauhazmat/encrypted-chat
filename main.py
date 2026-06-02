2ba4bb9f383bbeb37410b5aa651c9339548015f4
Tauha Azmat <160022078+tauhazmat@users.noreply.github.com>
2026-06-02 22:13:48 +0500

    Asymmetric encryption heading

--- main.py
+++ main.py
@@ -39,3 +39,6 @@ def receiving_messages(c):
 
 threading.Thread(target=sending_messages, args=(client,)).start()
 threading.Thread(target=receiving_messages, args=(client,)).start()
+
+
+# working on Asymmetric encryption (public-key cryptography)
